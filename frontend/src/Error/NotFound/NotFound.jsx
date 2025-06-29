30b3190e597f912fe0ad362d72f15482aaa9d352
Nicat-Mehtizade <nicat.mehdizadeh@gmail.com>
2025-06-30 03:44:33 +0400

    Feat | Fe | 404NotFound Page Added

--- frontend/src/Error/NotFound/NotFound.jsx
+++ frontend/src/Error/NotFound/NotFound.jsx
@@ -1,8 +1,39 @@
+import styles from "./index.module.css";
+import { NavLink } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div>
+      <div>
+        <div className={styles.background}>
+          <div className="absolute inset-0 bg-black/50 flex flex-col justify-center items-center">
+            <h1 className="text-white text-4xl font-semibold mb-4 uppercase">
+              Account
+            </h1>
+            <div className="text-white font-medium flex items-center gap-2">
+              <NavLink
+                className="transition duration-500 hover:text-purple-900"
+                to={"/"}
+              >
+                Home
+              </NavLink>
+              <p>/</p>
+              <p>404 Not Found</p>
+            </div>
+          </div>
+        </div>
+        <div className="max-w-[1100px] mx-auto py-20">
+          <div>
+            <h1 className="text-5xl font-medium mb-10">404 Page Not Found</h1>
+            <p className="text-lg font-medium">
+              The page you requested does not exist. Click here to continue
+              shopping.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
